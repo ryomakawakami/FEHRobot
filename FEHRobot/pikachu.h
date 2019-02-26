d2f5ee017ba0534bc66088dc45ed034c2654660a
ryomakawakami <ryomakwkm@gmail.com>
2019-02-26 14:16:07 -0500

    Adjusted Pikachu

--- FEHRobot/pikachu.h
+++ FEHRobot/pikachu.h
@@ -1,6 +1,12 @@
 #ifndef PIKACHU_H
 #define PIKACHU_H
 
+typedef struct {
+    char one;
+    char two;
+    char three;
+} hexcode;
+
 void drawPicture(int colors[], int size_x, int size_y, int pos_x, int pos_y) {
     for(int j = 0; j < size_y; j++) {
         for(int i = 0; i < size_x; i++) {
